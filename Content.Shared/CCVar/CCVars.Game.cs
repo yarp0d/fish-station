c34468356d572fffee7e788ab3baba47ab6bc594
Vigers Ray <vigersray@gmail.com>
2025-08-18 02:28:59 +0300

    опять насрали

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -88,7 +88,7 @@ public sealed partial class CCVars
     ///     Prototype to use for map pool.
     /// </summary>
     public static readonly CVarDef<string>
-        GameMapPool = CVarDef.Create("game.map_pool", "FishMapPool", CVar.SERVERONLY); //Fish-Edit
+        GameMapPool = CVarDef.Create("game.map_pool", "SunriseMapPool", CVar.SERVERONLY); // Sunrise-Edit
 
     /// <summary>
     ///     The depth of the queue used to calculate which map is next in rotation.
